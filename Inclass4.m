e8363afe95d6cd4d9ce9a7247eee2c769819980a
isahnoune <31284645+isahnoune@users.noreply.github.com>
2017-09-12 12:26:09 -0500

    Add files via upload

--- Inclass4.m
+++ Inclass4.m
@@ -6,6 +6,39 @@
 %that reads only the first line of random numbers in the file and stores
 %them as numbers in an array. 
 
+%a
+filename = 'InClass4Question1.txt';
+fid = fopen(filename, 'w');
+fprintf(fid, 'Random numbers 1');
+x = randi(10,13);
+for i = 1:10
+    fprintf(fid, "\n");
+    for k = 1:13
+        fprintf(fid, x(i,k)+"\t");
+        
+    end    
+end
+
+
+fprintf(fid, 'Random numbers 2 \n');
+y = randi(9,7);
+for j = 1:9
+    fprintf(fid, "\n");
+    for k = 1:7
+        fprintf(fid, x(i,k)+"\t");
+    end
+end
+
+%b
+
 % 2. Write a function that takes an array as input and returns a logical
 % variable which is true if the sum of the numbers in the array is greater
 % than or equal to 10 and false if it is less than 10. 
+
+function value = Practice(matrixcheck)
+temp = sum(matrixcheck);
+if temp < 10
+    value = 0;
+else
+    value = 1;
+end
